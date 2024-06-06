f2c9f03a4ab5a7cee3c6125445eb5d56dd89f6c0
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-06 11:20:37 +0300

    feat(dto): implement dto

--- src/main/java/com/example/TaskManager/dto/TaskDto.java
+++ src/main/java/com/example/TaskManager/dto/TaskDto.java
@@ -1,4 +1,26 @@
 package com.example.TaskManager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class TaskDto {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime dueDate;
+
+    private String status;
+
+    private UserDto assignee;
+
 }
--- src/main/java/com/example/TaskManager/dto/UserDto.java
+++ src/main/java/com/example/TaskManager/dto/UserDto.java
@@ -1,4 +1,20 @@
 package com.example.TaskManager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
 }
