d517dad439d9b88f359462cb17ec93ddf4d2004a
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-06 11:20:50 +0300

    feat(entity): implement entities

--- src/main/java/com/example/TaskManager/entity/Task.java
+++ src/main/java/com/example/TaskManager/entity/Task.java
@@ -1,4 +1,37 @@
 package com.example.TaskManager.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    private LocalDateTime dueDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
 }
--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -1,4 +1,27 @@
 package com.example.TaskManager.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
