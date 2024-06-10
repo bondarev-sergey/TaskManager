ee742f79890ed26b56f5954ddb2d0465052e6269
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-10 11:07:40 +0300

    feat(service): add UserDetailsServiceImpl

---
+++ src/main/java/com/example/TaskManager/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.TaskManager.service.impl;
+
+import com.example.TaskManager.entity.User;
+import com.example.TaskManager.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = optionalUser.get();
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+
+}
