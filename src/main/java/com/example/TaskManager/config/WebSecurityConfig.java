d16e8f4715d8a79b23f4bdd6773e0340f7064917
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-10 11:06:54 +0300

    feat: remove unused auth beans

--- src/main/java/com/example/TaskManager/config/WebSecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.TaskManager.config;
-
-public class WebSecurityConfig {
-}
--- src/main/java/com/example/TaskManager/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.TaskManager.service;
-
-import com.example.TaskManager.dto.UserDto;
-
-public interface AuthService {
-
-    UserDto authenticate(String email, String password);
-
-}
