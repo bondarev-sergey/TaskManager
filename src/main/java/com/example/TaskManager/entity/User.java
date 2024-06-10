2d9867b4a1759b47505e50cad047f33152df7acb
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-10 11:07:23 +0300

    feat(entity): add role to User

--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -14,14 +14,21 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String firstName;
 
     @Column(nullable = false)
     private String lastName;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
 }
