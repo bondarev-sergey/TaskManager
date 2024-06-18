03a20a3c0ab068ede4cdc1502c0ef048d6c00049
sergey <BondarevSA@admlr.lipetsk.ru>
2024-06-18 10:29:16 +0300

    feat(config): add cors configuration source

--- src/main/java/com/example/TaskManager/config/SecurityConfig.java
+++ src/main/java/com/example/TaskManager/config/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -24,10 +29,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/users/**").hasRole("ADMIN")
-                .requestMatchers("/tasks/**").authenticated()
-                .requestMatchers("/register").permitAll()
+        http.cors(cors -> corsConfigurationSource())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                        .requestMatchers("/tasks/**").authenticated()
+                        .requestMatchers("/register").permitAll()
         );
         http.httpBasic(Customizer.withDefaults());
         return http.build();
@@ -46,5 +52,17 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+        configuration.setAllowedHeaders(Collections.singletonList("Content-Type"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
