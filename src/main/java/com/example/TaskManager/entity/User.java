b1f9faba7e98d17cc29da650aa98a8f5e7b68e79
sergey <BondarevSA@admlr.lipetsk.ru>
2024-07-04 12:47:05 +0300

    refactor(entity): change property name for role

--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -14,7 +14,7 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
-    private String role;
+    private String roles;
 
     @Column(nullable = false, unique = true)
     private String email;
