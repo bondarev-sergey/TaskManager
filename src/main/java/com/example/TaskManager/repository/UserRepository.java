474b218d616eb79dc657b193a43a74845778381c
sergey <BondarevSA@admlr.lipetsk.ru>
2024-11-08 16:11:01 +0300

    feat(repo): change UserRepository

--- src/main/java/com/example/TaskManager/repository/UserRepository.java
+++ src/main/java/com/example/TaskManager/repository/UserRepository.java
@@ -3,10 +3,15 @@ package com.example.TaskManager.repository;
 import com.example.TaskManager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByOrderByUsernameAsc();
+
+    void deleteByUsername(String username);
 
 }
