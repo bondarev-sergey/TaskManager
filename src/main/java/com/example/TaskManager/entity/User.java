dfdc9abb0468ace541d4ef6fa8ca893c52e86fef
sergey <BondarevSA@admlr.lipetsk.ru>
2024-11-08 15:56:41 +0300

    feat(entity): change user entity

--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -13,17 +13,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String roles;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
     @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String authority;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 
     @Column(nullable = false)
     private String firstName;
